443cf15e35cb605e8db13d56e870d2ca5fb83f7f
Jared <55984417+bppwd2020-bourassa@users.noreply.github.com>
2020-05-21 16:29:32 -0400

    fixed prefix

--- emperor.js
+++ emperor.js
@@ -16,21 +16,23 @@ client.on('message', msg => {
   
   if(msg.content.substring(0,1) == (config.prefix)) {
 
-    if (msg.content === 'pog') {
+    console.log(config.prefix);
+
+    if (msg.content.substring(1) === 'pog') {
       msg.reply('champ');
     }
 
-    if (msg.content === 'me') {
+    if (msg.content.substring(1) === 'me') {
       msg.reply(msg.author.displayAvatarURL());
     }
 
-    if (msg.content === 'roll') {
+    if (msg.content.substring(1) === 'roll') {
       int = Math.floor((Math.random() * 20) + 1)
       msg.reply(int);
     }
 
-    if(msg.content === 'role') {
-
+    if(msg.content.substring(1) === 'role') {
+      
     }
   }
 });
